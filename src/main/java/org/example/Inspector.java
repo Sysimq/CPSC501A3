18201b011ed4f23c5d4badaca350b8a74e49dc1d
seyeonsim <sysim@ucalgary.ca>
2022-11-24 19:24:33 -0700

    completed array inspect methods and added some unit tests

--- src/main/java/org/example/Inspector.java
+++ src/main/java/org/example/Inspector.java
@@ -29,8 +29,7 @@ public class Inspector {
 
     //Get object class information
     public void getClassInfo(Class c, int depth){
-        print("CLASS INSEPCTION", depth);
-        print("Class: "+c.getName(), depth);
+        print("Class Name: "+c.getName(), depth);
     }
     //Get the object's super class information
     public void getSuperClassInfo(Class c, Object obj, boolean recursive, int depth){
@@ -91,15 +90,97 @@ public class Inspector {
         }
     }
     private void printMethodInfo(int depth, int t, Method m){
+        print("Method: "+m.getName(), depth);
+        Class[] exceptions = m.getExceptionTypes();
+        if (exceptions.length > 0){
+            for (Class e: exceptions){
+                print("Method Exceptions: "+e.getName(), t);
+            }
+        }
+        Class[] parameters = m.getParameterTypes();
+        if(parameters.length > 0){
+            for(Class p: parameters){
+                print("Method Parameter Types: "+p.getName(), t);
+            }
+        }
+        print("Method Return Type: " + m.getReturnType(), t);
+        String modifier = Modifier.toString(m.getModifiers());
+        print("Method Modifiers: " + modifier, t);
 
     }
     public void getFieldInfo(Class c, Object obj, boolean recursive, int depth){
+        Field[] fields = c.getDeclaredFields();
+        if(fields.length > 0){
+            for(Field f: fields){
+                printFieldInfo(obj, recursive, depth, depth + 1, f);
+            }
+        }
+    }
+    private void printFieldInfo(Object obj, boolean recursive, int depth, int t, Field f){
+        f.setAccessible(true);
+        print("Field Name: "+ f.getName(), depth);
+        Class type = f.getType();
+        print("Field Type: "+type.getSimpleName(),t);
+        String modifiers = Modifier.toString(f.getModifiers());
+        print("Field Modifiers: " + modifiers, t);
 
+        try{
+            Object fieldObj = f.get(obj);
+
+            if(fieldObj == null){
+                print("Value: null", t);
+            }
+            else if (type.isPrimitive()) {
+                print("Value: "+fieldObj.toString(), t);
+            }
+            else if (type.isArray()) {
+                getArrayInfo(f.getType(),fieldObj,recursive,t);
+            }
+            else{
+                if(recursive){
+                    inspectClass(type, fieldObj, true, t);
+                }
+                else{
+                    print("Reference Value: "+ fieldObj.getClass().getName()
+                            + "@" + Integer.toHexString(System.identityHashCode(obj)), t);
+                }
+            }
+        }catch(IllegalAccessException e){
+            e.printStackTrace();
+        }
     }
     public void getArrayInfo(Class c, Object obj, boolean recursive, int depth){
+        Class cType = c.getComponentType();
+        int t = depth +1;
+        int aLength = Array.getLength(obj);
 
+        print("Component Type: " + cType.getSimpleName(), depth + 1);
+        print("Array Length: " + aLength, depth + 1);
+
+        for (int i = 0; i < aLength; i++) {
+            Object arrayObj = Array.get(obj, i);
+
+            if (arrayObj == null) {
+                print(i +": null",t+1);
+            }
+            else if (cType.isPrimitive()) {
+                print(i +": "+ arrayObj.getClass().getName(), t+1);
+            }
+            else if (cType.isArray()) {
+                getArrayInfo(arrayObj.getClass(), arrayObj, recursive, t+1);
+            }
+            else {
+                if (recursive) {
+                    print(i + ": Array", t+1);
+                    inspectClass(arrayObj.getClass(), arrayObj, true, t+1);
+                }
+                else {
+                    print("Value: " + arrayObj.getClass().getName() + "@" + Integer.toHexString(System.identityHashCode(arrayObj)), t+1);
+                }
+            }
+
+        }
     }
-
 
     private void print(String output, int depth){
         for (int i = 0; i < depth; i++)
--- src/test/java/org/example/InspectorTest.java
+++ src/test/java/org/example/InspectorTest.java
@@ -4,19 +4,44 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class InspectorTest {
 
+    private static final ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+
     @BeforeEach
-    void setUp() {
+    public void setUp() {
+        System.setOut(new PrintStream(outStream));
     }
 
     @AfterEach
-    void tearDown() {
+    public void tearDown() {
+        outStream.reset();
     }
 
     @Test
-    void inspect() {
+    public void testClassName() {
+        new Inspector().inspect(new String(), false);
+        String expected = "\nClass Name: java.lang.String";
+        assert(outStream.toString().contains(expected));
     }
+
+    @Test
+    public void testWrongClassName(){
+        new Inspector().inspect(new String(), false);
+        String notExpected = "\nClass Name: String";
+        assertFalse(outStream.toString().contains(notExpected));
+    }
+    @Test
+    public void testSuperClassInfo(){
+        new Inspector().inspect(new String(), false);
+        String expected = "\nSuper Class Name: java.lang.Object";
+        assert(outStream.toString().contains(expected));
+
+    }
+
 }
